d847cca2a31104f8489c48a0a0b8b346bb7f8738
BharathGupta <BharathGupta@BharathGupta-PC>
2018-07-12 09:09:12 +0530

    FirstCommit

---
+++ MyFirstProject/src/day1/FirstClass.java
@@ -0,0 +1,22 @@
+package day1;
+
+import keywordsConcept.TC1;
+
+public class FirstClass extends TC1 {
+
+	
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		
+		FirstClass obj = new FirstClass();
+		System.out.println(obj.a);
+		
+		TC1 obj1 = new TC1();
+//		System.out.println(obj1.a);
+
+		
+		
+	}
+
+}
