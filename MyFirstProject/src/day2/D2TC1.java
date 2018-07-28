eba8cb84b4305106e87cd48150717601fd4c3173
Narsimha <Narsimha@NARSIMHA-PC>
2018-07-28 15:47:38 +0530

    Myfirstcommit

--- MyFirstProject/src/day2/D2TC1.java
+++ MyFirstProject/src/day2/D2TC1.java
@@ -3,35 +3,7 @@ package day2;
 public class D2TC1 {
 	
 	public static void main(String[] args){
-	/*
-		int a;
-		char t; // Declring
-		
-		a=10; //initilizing the value
-		
-		char A = '%'; // 
-//		System.out.println(A);
-		
-		float s = (float) 8.4;
-		
-//		System.out.println(s);
-		
-		
-		double r = 8.4;
-		
-//		System.out.println(r);
-		
-		int c=10;
-		int b=20;
-		
-		int add = c+b;
-		
-		
-		System.out.println(add);
-		
-		System.out.println("a5dfdklfjdklfjsdlkfjds");
-		*/
-		
+	
 
 		boolean flag = false;
 		
